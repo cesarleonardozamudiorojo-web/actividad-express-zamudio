b59f71ca45b2daf22240bc116847f27986906582
Zamudio Rojo Cesar Leonardo <cesarleonardozamudiorojo@gmail.com>
2026-04-18 17:15:54 -0700

    Primer SELECT de alumnos con PostgreSQL

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const pool = require('./db');
 const app = express();
 
+app.use(express.json());
+
+// Ruta base
 app.get('/', (req, res) => {
   res.send('API funcionando');
 });
 
-app.get('/usuario', (req, res) => {
-  const usuario = {
-    id: 1,
-    nombre: 'Cesar Leonardo Zamudio Rojo',
-    rol: 'Estudiante'
-  };
-
-  res.json(usuario);
+// 🔥 Ruta alumnos
+app.get('/alumnos', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM alumno');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar alumnos:', error);
+    res.status(500).json({ error: 'Error al obtener los alumnos' });
+  }
 });
 
-app.listen(8080, () => {
-  console.log('Servidor corriendo en http://127.0.0.1:8080');
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
 });
